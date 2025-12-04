bb9ea5cec436ac7f2b86be66f1be7cdd9822b30b
Sudikshaw7 <sudikshawandre@gmail.com>
2025-12-04 15:43:54 +0530

    commit changes for todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import viteLogo from "/to-do-list.svg";
+import "./App.css";
+import { Button, Container, ListGroup } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  const handleSubmit = () => {
+    if (!task.trim()) return;
+    const updateList = [...list, task];
+    setList(updateList);
+    console.log("first", updateList);
+    setTask("");
+  }
+
+  const editTask =(index) =>{
+    const update = prompt("edit task", list[index]);
+    if(update && update.trim()){
+      const updateList = [...list];
+      updateList[index] = update;
+      setList (updateList);
+      console.log("edit", updateList)
+    }
+  };
+
+  const deleteTask =(index) =>{
+    if (confirm("are you sure?")){
+    const updateList = list.filter((_,i)=>i !== index);
+    setList(updateList);
+    alert("data is deleted")
+  }
+};
+
+return (
+  <>
+    <Container>
+      <h1 className="app">To Do</h1>
+        <form>
+          <input
+            className="input"
+            type="text"
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
+          />
+          <Button
+            className="btn-primary"
+            variant="primary"
+            onClick={handleSubmit}
+          >Add</Button>
+        </form>
+<ul className="list">
+        <ListGroup className="mt-3">
+          {list.map((t, index) => (
+            
+            <ListGroup.Item
+              key={index}
+              className="list-group"
+            >{t}
+                <Button
+                onClick={() =>editTask(index)}
+                className="btn-secondary"
+                >Edit</Button>
+
+               <Button
+                onClick={() =>deleteTask(index)}
+                className="btn-warning"
+              >Delete</Button>
+
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        </ul>
+    </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
